18618a985b840de99d1dde0275dcd681d350d8d8
Lauryn White <white797@purdue.edu>
2024-09-29 21:53:55 -0400

    Appbar placeholder

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,20 +9,25 @@ import ExampleList from './example/ExampleList';
 import CreateReviewPage from './pages/reviews/CreateReviewPage';
 import SearchPage from './pages/search/SearchPage'
 import MediaPage from './pages/media/MediaPage'
+import { AppBar, Container, Toolbar, Typography } from '@mui/material';
+import CustomAppBar from './components/CustomAppBar';
 
 function App() {
   // const { isLoading, error } = useAuth0();
   return (
     <Router>
-      <Routes>
-        {/* Define the route for /account */}
-        <Route path="/" element={<Home />} />
-        <Route path="/account" element={<AccountPage />} />
-        <Route path="/createReview" element={<CreateReviewPage />} />
-        <Route path="/examples" element={<ExampleList />} />
-        <Route path="/search" element={<SearchPage />} />
-        <Route path="/media/:id" element={<MediaPage />} />
-      </Routes>
+      <CustomAppBar />
+      <Container sx={{ mt: 5 }}>
+        <Routes>
+          {/* Define the route for /account */}
+          <Route path="/" element={<Home />} />
+          <Route path="/account" element={<AccountPage />} />
+          <Route path="/createReview" element={<CreateReviewPage />} />
+          <Route path="/examples" element={<ExampleList />} />
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/media/:id" element={<MediaPage />} />
+        </Routes>
+      </Container>
     </Router>
   )
 }
