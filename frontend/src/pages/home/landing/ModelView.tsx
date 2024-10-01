42493e6f5d0ae8f3b534d5c58c94189bab37b044
Sean Choi <szeanchoi@gmail.com>
2024-09-30 21:11:19 -0400

    top text responsive to window height

--- frontend/src/pages/home/landing/ModelView.tsx
+++ frontend/src/pages/home/landing/ModelView.tsx
@@ -1,4 +1,4 @@
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
 import { useFrame } from '@react-three/fiber';
 import { useMatcapTexture, Text3D, useGLTF, Environment } from '@react-three/drei';
 import { useThree } from '@react-three/fiber';
@@ -7,6 +7,8 @@ import * as THREE from 'three'
 const matcapMat = new THREE.MeshMatcapMaterial();
 
 function ModelView() {
+  const [height, setHeight] = useState() as any;
+
   const book = useGLTF("/models/book.gltf");
   const slate = useGLTF("/models/slate.gltf");
   const vg = useGLTF("/models/vg.gltf");
@@ -22,29 +24,32 @@ function ModelView() {
   const rotationMult = .05;
   const positionMult = .05;
   const { mouse } = useThree(); // Get mouse position from the canvas
-  const [matcapTexture] = useMatcapTexture('2D2D2F_C6C2C5_727176_94949B', 512)
+  const [matcapTexture] = useMatcapTexture('28292A_D3DAE5_A3ACB8_818183', 512)
 
   useEffect(() => {
     matcapTexture.colorSpace = THREE.SRGBColorSpace
     matcapTexture.needsUpdate = true
     matcapMat.matcap = matcapTexture
     matcapMat.needsUpdate = true
+
+    window.addEventListener('resize', setHeight(window.innerHeight));
+    return () => window.removeEventListener('resize', setHeight(window.innerHeight));
   }), []
 
   // Update the text position or rotation based on mouse movement
   useFrame(() => {
     if (booksRef.current) {
-      booksRef.current.rotation.y = -Math.PI/24 + (-mouse.x * Math.PI * rotationMult);
+      booksRef.current.rotation.y = -Math.PI / 24 + (-mouse.x * Math.PI * rotationMult);
     }
     if (vgRef.current) {
-      vgRef.current.rotation.y = -Math.PI/12 + mouse.x * Math.PI * rotationMult;
+      vgRef.current.rotation.y = -Math.PI / 12 + mouse.x * Math.PI * rotationMult;
     }
     if (mvtvRef.current) {
-      mvtvRef.current.rotation.y = Math.PI/12 + mouse.x * Math.PI * rotationMult;
+      mvtvRef.current.rotation.y = Math.PI / 12 + mouse.x * Math.PI * rotationMult;
     }
     if (openBookRef.current) {
-      openBookRef.current.rotation.x = Math.PI/10 + mouse.y * Math.PI * rotationMult;
-      openBookRef.current.rotation.z = Math.PI/9 + mouse.x * Math.PI * rotationMult;
+      openBookRef.current.rotation.x = Math.PI / 10 + mouse.y * Math.PI * rotationMult;
+      openBookRef.current.rotation.z = Math.PI / 9 + mouse.x * Math.PI * rotationMult;
 
       openBookRef.current.position.x = 1 + mouse.x * positionMult;
       openBookRef.current.position.y = -1.7 + mouse.y * positionMult;
@@ -57,7 +62,7 @@ function ModelView() {
       controllerRef.current.position.y = .7 + mouse.y * positionMult;
     }
     if (slateRef.current) {
-      slateRef.current.rotation.x = Math.PI / 2- mouse.y * Math.PI * rotationMult;
+      slateRef.current.rotation.x = Math.PI / 2 - mouse.y * Math.PI * rotationMult;
       slateRef.current.rotation.z = mouse.x * Math.PI * rotationMult;
 
       slateRef.current.position.x = -1 + mouse.x * positionMult;
@@ -65,6 +70,7 @@ function ModelView() {
     }
     if (trackRef.current) {
       trackRef.current.rotation.x = mouse.y * Math.PI * .1;
+      trackRef.current.position.y = 1 + height/2000;
     }
   });
 
@@ -118,7 +124,7 @@ function ModelView() {
       <Text3D
         material={matcapMat}
         position={[-1.7, 0, 0]}
-        rotation-x={-Math.PI/7}
+        rotation-x={-Math.PI / 7}
         font="/fonts/open_extrabold.json"
         letterSpacing={-.05}
         height={.25}
