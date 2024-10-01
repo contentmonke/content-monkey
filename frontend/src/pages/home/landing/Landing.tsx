d87ffc52923a7cdf01cdb3eea762047544d0214d
Sean Choi <szeanchoi@gmail.com>
2024-09-30 20:53:16 -0400

    home page with models done

--- frontend/src/pages/home/landing/Landing.tsx
+++ frontend/src/pages/home/landing/Landing.tsx
@@ -1,15 +1,40 @@
+import './Landing.css'
+
 import cmLogo from '/monkey.svg'
 
 import SignUpButton from '../SignUpButton/SignUpButton';
-import { useAuth0 } from '@auth0/auth0-react';
+import { Canvas } from '@react-three/fiber';
+import { Loader } from '@react-three/drei';
+import ModelView from './ModelView.tsx'
+import { Suspense } from 'react';
 
 function Landing() {
-  const { loginWithRedirect } = useAuth0();
 
   return <>
-    <h1 className="title">CONTENT</h1>
-    <img className="logo" src={cmLogo} />
-    <h1 className="title">MONKEY</h1>
+    <Canvas
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+      }}
+      style={{ position: 'absolute', top: 0, left: 0, zIndex: 1 }}
+    >
+      <Suspense fallback={null}>
+        <ModelView />
+      </Suspense>
+    </Canvas>
+    <Loader />
+
+    <div className="gradient"></div>
+
+    <div className="cont">
+      <div className="title">
+        <span className="left-text">CONTENT</span>
+        <img className="logo" src={cmLogo} />
+        <span className="right-text">MONKEY </span>
+      </div>
+    </div>
+
     <SignUpButton />
   </>;
 }
