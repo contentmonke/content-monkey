fd8d413c668d15d749e83659735c36994d620722
Lauryn White <white797@purdue.edu>
2024-10-02 06:11:39 -0400

    More changes

--- frontend/src/pages/reviews/ModalHeader.tsx
+++ frontend/src/pages/reviews/ModalHeader.tsx
@@ -2,22 +2,12 @@ import { Container, IconButton } from "@mui/material";
 import { modalHeader } from "../../style/review-page";
 import { Close } from "@mui/icons-material";
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
-import OpenInFullIcon from '@mui/icons-material/OpenInFull';
 
-function ModalHeader({ media, setModalOpen, setShowWarning, handleExpandScreenClick }: any) {
+function ModalHeader({ media, setModalOpen, setShowWarning }: any) {
   return (
     <Container disableGutters sx={{ ...modalHeader }}>
-      {media === null ?
-        <IconButton onClick={() => setModalOpen(false)}>
-          <Close />
-        </IconButton>
-        :
-        <IconButton onClick={() => setShowWarning(true)}>
-          <ArrowBackIosNewIcon />
-        </IconButton>
-      }
-      <IconButton onClick={handleExpandScreenClick}>
-        <OpenInFullIcon />
+      <IconButton onClick={() => setShowWarning(true)}>
+        <Close />
       </IconButton>
     </Container>
   );
