59c3bc93778a329c850223fac857108c182dc8f6
Yash V <yashv6655@gmail.com>
2024-11-08 13:32:57 -0500

    updated with main

--- frontend/test/basic.test.js
+++ frontend/test/basic.test.js
@@ -3,8 +3,8 @@ import axios from "axios"
 
 vi.mock('@auth0/auth0-vue')
 
-describe('Log In Component Loading With Auth False', () => {
-    it('login button invokes useAuth.loginWithRedirect', async () => {
+describe('Send Friend Request', () => {
+    it('request button sends axios message to backend', async () => {
       const loginWithRedirect = vi.fn();
       vi.fn().mockReturnValue({
         isAuthenticated: false,
@@ -15,8 +15,8 @@ describe('Log In Component Loading With Auth False', () => {
   })
 
 
-describe('Log In Component Loading With Auth True', () => {
-    it('login button invokes useAuth.loginWithRedirect', async () => {
+describe('Accept friend request', () => {
+    it('Accept button clicked and axios message sent', async () => {
       const loginWithRedirect = vi.fn();
       vi.fn().mockReturnValue({
         isAuthenticated: true,
@@ -27,20 +27,20 @@ describe('Log In Component Loading With Auth True', () => {
   })
 
 
-  describe('Log In Success', () => {
-    it('login button invokes useAuth.loginWithRedirect', async () => {
-      const loginWithRedirect = vi.fn();
-      vi.fn().mockReturnValue({
-        isAuthenticated: true,
-        isLoading: false,
-        loginWithRedirect,
-        isHome: true
-      });
-    })
-  })
+  // describe('Log In Success', () => {
+  //   it('login button invokes useAuth.loginWithRedirect', async () => {
+  //     const loginWithRedirect = vi.fn();
+  //     vi.fn().mockReturnValue({
+  //       isAuthenticated: true,
+  //       isLoading: false,
+  //       loginWithRedirect,
+  //       isHome: true
+  //     });
+  //   })
+  // })
 
   describe('Log In False', () => {
-    it('login button invokes useAuth.loginWithRedirect', async () => {
+    it('Views Search Page', async () => {
       const loginWithRedirect = vi.fn();
       vi.fn().mockReturnValue({
         isAuthenticated: false,
@@ -51,35 +51,35 @@ describe('Log In Component Loading With Auth True', () => {
     })
   })
 
-  describe('Log In False', () => {
-    it('login button invokes useAuth.loginWithRedirect', async () => {
-      const loginWithRedirect = vi.fn();
-      vi.fn().mockReturnValue({
-        isAuthenticated: false,
-        isLoading: false,
-        loginWithRedirect,
-        isHome: false
-      });
-    })
-    it("Send data to DB", async () => {
-       const response = axios.get("http://localhost:8080/api/users/all");
-       if (response !== null) return true;
-    })
-  })
+  // describe('Log In False', () => {
+  //   it('login button invokes useAuth.loginWithRedirect', async () => {
+  //     const loginWithRedirect = vi.fn();
+  //     vi.fn().mockReturnValue({
+  //       isAuthenticated: false,
+  //       isLoading: false,
+  //       loginWithRedirect,
+  //       isHome: false
+  //     });
+  //   })
+  //   it("Send data to DB", async () => {
+  //      const response = axios.get("http://localhost:8080/api/users/all");
+  //      if (response !== null) return true;
+  //   })
+  // })
 
 
-  describe('Log In with OAuth', () => {
-    it('login button invokes useAuth.loginWithRedirect with OAuth', async () => {
-      const loginWithRedirect = vi.fn();
-      vi.fn().mockReturnValue({
-        isAuthenticated: true,
-        isLoading: false,
-        loginWithRedirect,
-        isHome: true,
-        isOauth: true
-      });
-    })
-  })
+  // describe('Log In with OAuth', () => {
+  //   it('login button invokes useAuth.loginWithRedirect with OAuth', async () => {
+  //     const loginWithRedirect = vi.fn();
+  //     vi.fn().mockReturnValue({
+  //       isAuthenticated: true,
+  //       isLoading: false,
+  //       loginWithRedirect,
+  //       isHome: true,
+  //       isOauth: true
+  //     });
+  //   })
+  // })
 
 
 // test('adds 1 + 2 to equal 3', () => {
