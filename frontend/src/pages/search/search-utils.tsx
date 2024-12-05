d0662bf7c2f69329e09df5f0e12cf83c75fbdb2b
Drew <dgerrits@purdue.edu>
2024-12-05 16:21:04 -0500

    make search nicer to use

--- frontend/src/pages/search/search-utils.tsx
+++ frontend/src/pages/search/search-utils.tsx
@@ -4,80 +4,18 @@ import { MediaType } from "../../models/Models";
 
 export async function loadSearchResults(mediaType: string, title: string, setResults: any, setIsLoading: any, setIsError: any) {
   setIsLoading(true)
-  if (mediaType === MediaType.BOOK) {
-    api.search.fetchSearch({ title })
-      .then((response) => {
-        console.log(response.data)
-        setResults(response.data)
-        setIsError(false);
-      })
-      .catch(() => {
-        console.error("Error fetching search results");
-        setIsError(true);
-      })
-      .finally(() => {
-        setIsLoading(false);
-      })
-  }
-  else if (mediaType == MediaType.MOVIE) {
-    api.searchMovie.fetchMoviesSearch( title )
-      .then((response) => {
-        console.log(response.data)
-        setResults(response.data)
-        setIsError(false);
-      })
-      .catch(() => {
-        console.error("Error fetching search results");
-        setIsError(true);
-      })
-      .finally(() => {
-        setIsLoading(false);
-      })
-  }
-  else if (mediaType == MediaType.TV_SHOW) {
-    api.searchTV.fetchTVSearch( title )
-      .then((response) => {
-        console.log(response.data)
-        setResults(response.data)
-        setIsError(false);
-      })
-      .catch(() => {
-        console.error("Error fetching search results");
-        setIsError(true);
-      })
-      .finally(() => {
-        setIsLoading(false);
-      })
-  }
-  else if (mediaType == MediaType.VIDEO_GAME) {
-    api.searchVideoGames.fetchVideoGameSearch( title )
-      .then((response) => {
-        console.log(response.data)
-        setResults(response.data)
-        setIsError(false);
-      })
-      .catch(() => {
-        console.error("Error fetching search results");
-        setIsError(true);
-      })
-      .finally(() => {
-        setIsLoading(false);
-      })
-  }
-  else if (mediaType == MediaType.UNSELECTED) {
-    api.searchAll.fetchAllSearch( title )
-      .then((response) => {
-        console.log(response.data)
-        setResults(response.data)
-        setIsError(false);
-      })
-      .catch(() => {
-        console.error("Error fetching search results");
-        setIsError(true);
-      })
-      .finally(() => {
-        setIsLoading(false);
-      })
-  }
+  api.searchAll.fetchAllSearch(title)
+    .then((response) => {
+      console.log(response.data)
+      setResults(response.data)
+      setIsError(false);
+    })
+    .catch(() => {
+      console.error("Error fetching search results");
+      setIsError(true);
+    })
+    .finally(() => {
+      setIsLoading(false);
+    })
   return [];
 }
